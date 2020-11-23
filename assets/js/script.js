e6f728485f2a04262319811ea22860848e86e18b
Veronica Martinez <vmartinez6615@gmail.com>
2020-11-22 20:50:13 -0500

    getting there slowly...

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/* When we search for a city, what is displayed is: 
+/* define variables: 
     - 5 day forecast
     - temperature
     - humidty
@@ -7,11 +7,22 @@
     - current weather
     - dates for the week 
     - weather icons 
-    -save searched cities */
+    - saved search history */
 
+var searchFormEl = document.querySelector("#form-input");
+var cityInputEl = document.querySelector("#searchTerm");
+var cityDisplayName = document.querySelector("#city");
+var currentDay = moment();
+var apiKey = "5845809d7889f7a45fda45775e2e9a45";
+var history = [];
+
+//weather api
 function currentDay() {
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=Denver&units=imperial&appid=49a151cb225928a5da31670d3c434d1e"
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=" +
+      apiKey
   )
     .then(function (response) {
       return response.json();
@@ -19,16 +30,36 @@ function currentDay() {
     .then(function (data) {
       document.querySelector("#currentWeather").innerHTML = `
       <h2>${data.name}</h2>
-      <h2>${new Date()}</h2>
-      <h4>Current Weather: ${data.main.temp}F</h4>
-      <h4>Wind: ${data.wind.speed}MPH</h4>
-      <h4>Humidity: ${data.main.humidity}</h4>
-      <h4>UV Index: 2.5</h4>`;
-      console.log(data.coord.lon);
+      <h4>${new Date()}</h4>
+      <p>Current Weather: ${data.main.temp}F</p>
+      <p>Wind: ${data.wind.speed}MPH</p>
+      <p>Humidity: ${data.main.humidity}</p>
+      <p>UV Index: 2.5</p>`; // firgure out how to get uv index....
+      console.log(data);
       weeklyForecast(data.coord.lon, data.coord.lat);
+      // city api?
+      if (!cityInputEl) {
+        return fetch(
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            lattitude +
+            "&lon=" +
+            longitude +
+            "&exclude={part}&appid=" +
+            apiKey
+        );
+      }
     });
 }
-currentDay();
-function weeklyForecast(lon, lat) {
-  console.log(lon, lat);
+
+function uv() {
+  if (current.uvi < 3) {
+    tempHTML += "low-uv text-light";
+  } else if (current.uvi < 6) {
+    tempHTML += "moderate-uv text-dark";
+  } else if (current.uvi < 8) {
+    tempHTML += "high-uv text-dark";
+  } else if (current.uvi < 11) {
+    tempHTML += "very-high-uv text-light";
+  }
 }
+//so lost...
