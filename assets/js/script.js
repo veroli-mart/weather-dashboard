d763d905aa04bab9c7455582b64568c1ab5529d9
Veronica Martinez <vmartinez6615@gmail.com>
2020-12-19 20:38:22 -0500

    final product...forgot to save after many days of working on it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,126 @@
-/* define variables: 
-    - 5 day forecast
-    - temperature
-    - humidty
-    - wind speed
-    - uv index
-    - current weather
-    - dates for the week 
-    - weather icons 
-    - saved search history */
+var apiKey = "bf14cfed992dff4bbd7f61707d5e2756";
 
-var searchFormEl = document.querySelector("#form-input");
-var cityInputEl = document.querySelector("#searchTerm");
-var cityDisplayName = document.querySelector("#city");
-var todaysDate = moment();
-var apiKey = "";
-var history = [];
+var date = moment().format("MM/DD/YYYY");
 
-function findCity() {
-  $(searchTerm);
-}
-//weather api
-function currentDay() {
+//submit button
+var submitButtonEl = document.querySelector("#submit-btn");
+
+var storageLoad = false;
+
+//submit button event listener
+submitButtonEl.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  var cityInput = document.querySelector("#userCity").value.trim();
+
+  currentWeatherDisplay(cityInput);
+  saveCity(cityInput);
+});
+
+//function for current weather
+function currentWeatherDisplay(cityInput) {
   fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      city +
-      "&appid=e2c6cdeb7507c8fc17c433e8885d8d56"
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=${apiKey}`
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      document.querySelector("#currentWeather").innerHTML = `
-      <h2>Philadelphia</h2>
-      <h4>11/22/202</h4>
-      <p>Current Weather: 50F</p>
-      <p>Wind: 10MPH</p>
-      <p>Humidity: 10%</p>
-      <p>UV Index: 2.5</p>`; // firgure out how to get uv index....
       console.log(data);
-      // weeklyForecast(data.coord.lon, data.coord.lat);
-      // city api?
-      // if (!cityInputEl) {
-      //   return fetch(
-      //     "https://api.openweathermap.org/data/2.5/onecall?lat=&lon=&exclude={part}&appid=e2c6cdeb7507c8fc17c433e8885d8d56"
-      //     // //lattitude +
-      //     //  +
-      //     // //longitude +
-      //     // "" +
-      //     // apiKey
-      //   );
-      // }
+
+      document.querySelector("#currentWeather").innerHTML = `
+        <h3>${data.name} - ${date}</h3>
+        <p>Temperature: ${data.main.temp} F </p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} mph</p>
+        
+       `;
+
+      let lat = data.coord.lat;
+      let lon = data.coord.lon;
+      weekDisplay(lat, lon);
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${apiKey}`
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data2) {
+          document.querySelector(
+            "#currentWeather"
+          ).innerHTML += `<p>UV Index: ${data2.current.uvi}</p>`;
+        });
     });
 }
-currentDay();
 
-//
-function uv() {
-  if (current.uvi < 3) {
-    tempHTML += "low-uv text-light";
-  } else if (current.uvi < 6) {
-    tempHTML += "moderate-uv text-dark";
-  } else if (current.uvi < 8) {
-    tempHTML += "high-uv text-dark";
-  } else if (current.uvi < 11) {
-    tempHTML += "very-high-uv text-light";
-  }
+//function for 5 day forecast
+function weekDisplay(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=imperial&appid=${apiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      document.querySelector("#weeklyForecast").innerHTML = "";
+      for (var i = 1; i < 6; i++) {
+        //display 5 day forecast
+        document.querySelector("#weeklyForecast").innerHTML += `
+            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+              <div class="card-header">${moment
+                .unix(data.daily[i].dt)
+                .format("MM/DD/YYYY")} </div>
+              <div class="card-body">
+              <h5 class="card-title"><img src="http://openweathermap.org/img/wn/${
+                data.daily[i].weather[0].icon
+              }@2x.png" /> </h5>
+              <p class= "card-text"> Temp: ${data.daily[i].temp.day}</p>
+              <p class= "card-text"> Humidity: ${data.daily[i].humidity}</p>
+                </div>
+              `;
+      }
+    });
 }
 
-history = JSON.parse("");
-localStorage.getItem("history");
-localStorage.setItem("history", JSON.stringify(history));
-//so lost...
+function loadPreviousCities() {
+  document.querySelector("#userCity").innerHTML = "";
+  var citiesArray = JSON.parse(localStorage.getItem("previousCities"));
+  if (storageLoad === false) {
+    citiesArray.forEach((city) => {
+      document.querySelector(
+        "#cityList"
+      ).innerHTML += `<li class="list-group-item userItem">${city}</li>`;
+    });
+    storageLoad = true;
+  } else {
+    let citiesArrayLength = citiesArray.length - 1;
+    document.querySelector(
+      "#cityList"
+    ).innerHTML += `<li class="list-group-item userItem">${citiesArray[citiesArrayLength]}</li>`;
+  }
+  document.querySelectorAll(".userItem").forEach((city) => {
+    city.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      var cityInput = this.textContent;
+
+      currentWeatherDisplay(cityInput);
+    });
+  });
+}
+
+function saveCity(cityInput) {
+  //check if there's anything in local storage
+  if (!localStorage.getItem("previousCities")) {
+    localStorage.setItem("previousCities", JSON.stringify([]));
+  }
+  //take value of the city input
+  var citiesArray = JSON.parse(localStorage.getItem("previousCities"));
+  citiesArray.push(cityInput);
+
+  //save back to local storage
+  localStorage.setItem("previousCities", JSON.stringify(citiesArray));
+  loadPreviousCities();
+}
+
+loadPreviousCities();
